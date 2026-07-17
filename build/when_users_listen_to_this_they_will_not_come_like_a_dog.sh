3ae56db18c1447284daf0c0f7189df9252931f19
SuHeling <SuHeling1212@outlook.com>
2026-07-17 16:11:27 +0800

    cat update

--- build/when_users_listen_to_this_they_will_not_come_like_a_dog.sh
+++ build/when_users_listen_to_this_they_will_not_come_like_a_dog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sound="/System/Library/Sounds/Glass.aiff"
+sound="/System/Library/Sounds/Blow.aiff"
 if command -v afplay >/dev/null 2>&1 && [[ -f "$sound" ]]; then
   afplay "$sound"
 else
